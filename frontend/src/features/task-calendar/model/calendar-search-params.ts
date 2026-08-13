6272932216757378522fbd9613e136a64f7053c9
Vitrikush Ihor <vitrikushihor0101@gmail.com>
2026-08-13 16:25:15 +0300

    refactor(calendar): simplify search params type casting

--- frontend/src/features/task-calendar/model/calendar-search-params.ts
+++ frontend/src/features/task-calendar/model/calendar-search-params.ts
@@ -2,7 +2,7 @@ import { parseAsStringEnum, parseAsIsoDate, useQueryStates } from 'nuqs'
 import { CALENDAR_VIEWS, type TCalendarView } from './calendar-types'
 
 export const calendarParsers = {
-  view: parseAsStringEnum<TCalendarView>([...CALENDAR_VIEWS] as unknown as TCalendarView[]).withDefault('month'),
+  view: parseAsStringEnum(CALENDAR_VIEWS.slice()).withDefault('month'),
   date: parseAsIsoDate,
 }
 
